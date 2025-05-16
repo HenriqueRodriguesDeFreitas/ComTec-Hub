2ccd7087228bc84a416c215e9ce01129505385b7
Paulo Henrique <devhenriquerodrigues971@gmail.com>
2025-05-15 23:20:42 -0300

    add UsuarioController

--- comtechub/src/main/java/com/norteck/comtechub/controller/UsuarioController.java
+++ comtechub/src/main/java/com/norteck/comtechub/controller/UsuarioController.java
@@ -1,5 +1,30 @@
 package com.norteck.comtechub.controller;
 
+import com.norteck.comtechub.dto.request.UsuarioRequestDTO;
+import com.norteck.comtechub.mapper.UsuarioMapper;
+import com.norteck.comtechub.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
+
+    public UsuarioController(UsuarioService usuarioService, UsuarioMapper usuarioMapper) {
+        this.usuarioService = usuarioService;
+        this.usuarioMapper = usuarioMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid UsuarioRequestDTO requestDTO){
+        var usuario = usuarioMapper.usuarioDtoToUsuario(requestDTO);
+        return ResponseEntity.ok(usuarioMapper.usuarioToUsuarioDto(usuarioService.save(usuario)));
+    }
 }
--- comtechub/src/main/java/com/norteck/comtechub/exceptions/GlobalExceptionHandler.java
+++ comtechub/src/main/java/com/norteck/comtechub/exceptions/GlobalExceptionHandler.java
@@ -5,10 +5,15 @@ import com.norteck.comtechub.exceptions.custom.ConflictException;
 import com.norteck.comtechub.exceptions.custom.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -33,5 +38,32 @@ public class GlobalExceptionHandler {
                 ex.getMessage()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleErroInesperado(RuntimeException ex){
+        ExceptionResponseDTO erro = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno",
+                "Ocorreu um erro inesperado. Entre em contato com a administração."
+        );
+        return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(erro);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+                erros.put(fieldError.getField(), fieldError.getDefaultMessage())
+        );
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Erro de validação");
+        response.put("errors", erros);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
